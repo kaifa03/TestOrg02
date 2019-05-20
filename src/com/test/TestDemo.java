790c0b98c8edba7c8f1af385c6c94d954ead4497
lisi <lisi@qq.com>
2019-05-21 07:41:07 +0800

    关琳迁

--- src/com/test/TestDemo.java
+++ src/com/test/TestDemo.java
@@ -7,5 +7,6 @@ package com.test;
 public class TestDemo {
     public static void main(String[] args) {
         System.out.println("sssssssssssssssss");
+        System.out.println("你好");
     }
 }
